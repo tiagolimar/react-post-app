a67f88db3cb74dcd0556cdb157e2eaf7c55e4b33
tiagolimar <tiagolima.r2@gmail.com>
2024-11-07 20:35:46 -0300

    create a footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
-import Navbar from '../../components/Navbar.jsx';
 import Typography from '@mui/material/Typography';
+import Navbar from '../../components/Navbar.jsx';
+import Footer from '../../components/Footer.jsx';
 
 export const Home = () => {
   return (
@@ -8,6 +9,7 @@ export const Home = () => {
       <Typography variant="h1" component="h1" sx={{ textAlign: 'center', marginTop: "1em" }}>
         Home
       </Typography>
+      <Footer />
     </>
   )
 }
--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,13 +1,15 @@
-import Navbar from '../../components/Navbar.jsx';
 import Typography from '@mui/material/Typography';
+import Navbar from '../../components/Navbar.jsx';
+import Footer from '../../components/Footer.jsx';
 
 export const Post = () => {
   return (
     <>
-    <Navbar />
-    <Typography variant="h1" component="h1" sx={{ textAlign: 'center', marginTop: "1em" }}>
-        Post
+      <Navbar />
+      <Typography variant="h1" component="h1" sx={{ textAlign: 'center', marginTop: "1em" }}>
+          Post
       </Typography>
+      <Footer />
     </>
   )
 }
