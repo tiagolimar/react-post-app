718029856cc2423a8100b412b80ad77ed5d7d947
tiagolimar <tiagolima.r2@gmail.com>
2024-11-07 19:10:58 -0300

    config routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {Home} from './pages/home/Home.jsx'
+import {Login} from './pages/login/Login.jsx'
+import {Post} from './pages/post/Post.jsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/home",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/post",
+        element: <Post />,
+    },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+import Navbar from '../components/Navbar';
+
+export const Home = () => {
+  return (
+    <Navbar></Navbar>
+    <div>Home</div>
+  )
+}
